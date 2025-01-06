a4c83dda94a35709a0ab421a24862e375ae38800
Khaled Shaaban <khaled.shabaan277@gmail.com>
2025-01-06 18:16:45 +0300

    Added dynamic algorithm for items less than 6

--- api/app/Http/Controllers/KnapsackController.php
+++ api/app/Http/Controllers/KnapsackController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Genetic;
+use App\BruteForce;
 
 
 class KnapsackController extends Controller
@@ -12,9 +13,60 @@ class KnapsackController extends Controller
     $data = request()->all();
   
     $w = $data['allowedWeight']; // knapsack capacity
+
+    if (count($data['items']) > 6) {
+      $response = $this->solveUsingGenetic($data);
+    } else {
+      $response = $this->solveUsingDynamic($data);
+    }
     
+    return response()->json($response);
+  }
+
+  private function solveUsingDynamic(array $data)
+  {
+    //w: knapsack space
+    $bitCount = count($data['items']);
+    $max_profit = -1; $curr_profit = -1;
+    $max_profitWeight = -1; $curr_weight = -1;
+    $best_solution = '';
+    $charArray = array_fill(0, $bitCount, '1');
+    $maximum = bindec(implode('', $charArray));
+    
+    //This loop to try every solution and find the best profits that fit w: space of knapsack
+    for ($i = 0; $i <= $maximum; $i++){
+        $binValue = decbin($i);
+        $solution = BruteForce::completeToN($binValue, $bitCount);
+        $curr_profit = BruteForce::getProfit($solution, $data['items']);
+        $curr_weight = BruteForce::getWeight($solution, $data['items']);
+
+        if ($curr_weight <= $data['allowedWeight']){
+            if ($curr_profit > $max_profit){
+                $max_profit = $curr_profit;
+                $max_profitWeight = $curr_weight;
+                $best_solution = $solution;
+            }
+        }
+    }
+    
+    BruteForce::$bestSolution = $best_solution;
+    BruteForce::$maxProfit = $max_profit;
+    BruteForce::$maxProfitWeight = $max_profitWeight;
+
+    $profit = BruteForce::$maxProfit;
+    $weight = BruteForce::$maxProfitWeight;
+
+    return [
+      'profit' => $profit,
+      'weight' => $weight,
+      'solution' => $best_solution
+    ];
+  }
+
+  private function solveUsingGenetic(array $data)
+  {
     Genetic::setItems($data['items']);
-    Genetic::setW($w);
+    Genetic::setW($data['allowedWeight']);
     Genetic::initialPopulation();
 
     $parents = [];
@@ -32,11 +84,11 @@ class KnapsackController extends Controller
   
     $profit = Genetic::$maxProfit;
     $weight = Genetic::$maxProfitWeight;
-    
-    return response()->json([
+
+    return [
       'profit' => $profit,
       'weight' => $weight,
       'solution' => $bestSolution
-    ]);
+    ];
   }
 }
